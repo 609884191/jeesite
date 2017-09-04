b9494f6f98492206f3f65314e674eeb4b39d8c9a
609884191 <cwkj@172.23.234.3>
2017-09-04 17:34:50 +0800

    2017-09-04 17:34

--- jeesite-master/src/test/java/com/thinkgem/jeesite/test/Test.java
+++ jeesite-master/src/test/java/com/thinkgem/jeesite/test/Test.java
@@ -1,9 +1,16 @@
 package com.thinkgem.jeesite.test;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
 		
 		System.out.println("aaaaaab");
+	}
+	
+	public void printDate(){
+		
+		System.out.println(new Date().toString());
 	}
 }
